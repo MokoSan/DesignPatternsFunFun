8cc7608e12ae5e94ecf804ba175d4d067d405593
MokoSan <mukund.sharma92@gmail.com>
2017-04-15 18:18:51 -0400

    Prototype Pattern Added

---
+++ DesignPatternsFunFun/CreationalPatterns/Prototype.fs
@@ -0,0 +1,29 @@
+(* Specify the kind of objects to create using a prototypical instance, and create new objects by copying this prototype. *)
+
+module Prototype = 
+
+    [<Literal>]
+    let DesignPatternName = "Prototype"
+
+    type IPrototype  =
+        inherit System.ICloneable
+        abstract Name : string 
+
+    [<Sealed>]
+    type PrototypeA( name : string ) =
+        interface IPrototype with
+            member __.Name    = name 
+            member __.Clone() = PrototypeA( name ) :> obj
+
+    [<Sealed>]
+    type PrototypeB( name : string ) = 
+        interface IPrototype with
+            member __.Name    = name 
+            member __.Clone() = PrototypeB( name ) :> obj
+
+    let main() =
+        let prototypeA : IPrototype = ( PrototypeA( "Link ") :> IPrototype )
+        printfn "%s: Creating new Prototype of Prototype A with Name: %s" DesignPatternName prototypeA.Name 
+        let prototypeB : IPrototype = ( PrototypeB( "Zelda" ) :> IPrototype )
+        printfn "%s: Creating new Prototype of Prototype B with Name: %s" DesignPatternName prototypeB.Name 
+        0
