836f6b75ad44c210ab4c4ca6a12d39be54a2c68e
MokoSan <mukund.sharma92@gmail.com>
2017-04-15 22:23:17 -0400

    Added the Proxy Pattern

---
+++ DesignPatternsFunFun/StructuralPatterns/Proxy.fs
@@ -0,0 +1,28 @@
+(* Provide a surrogate or placeholder for another object to control access to it. *)
+
+module Proxy = 
+
+    [<Literal>]
+    let DesignPatternName = "Proxy"
+
+    type ISubject =
+        abstract member Operate : unit -> unit
+
+    [<Sealed>]
+    type ConcreteSubject() = 
+        interface ISubject with
+            member __.Operate() =
+                printfn "%s: Operate() from ConcreteSubject." DesignPatternName
+    
+    [<Sealed>]
+    type SubjectProxy( subject : ISubject ) =
+        let subject : ISubject = subject 
+        interface ISubject with
+            member __.Operate() =
+                printfn "%s: Operate() from SubjectProxy." DesignPatternName
+                subject.Operate()
+
+    let main() =
+        let subject : ISubject = ConcreteSubject() :> ISubject 
+        let subjectProxy       = SubjectProxy( subject )
+        ( subjectProxy :> ISubject ).Operate()
